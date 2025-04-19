36ad57b3c9ab89ad845919905f2e005d80c6ebb9
Ernest Guevarra <ernest@guevarra.io>
2025-04-19 22:00:36 +0100

    add hcw archive forms workflow; fix #5

---
+++ R/release_forms.R
@@ -0,0 +1,168 @@
+#'
+#' @param .year 
+#' @param major
+#'
+
+paglaom_create_release_list <- function(.year = lubridate::year(Sys.Date()),
+                                        major) {
+  jan_first <- as.Date(paste0(year, "-01-01"))
+  first_sunday <- jan_first + (7 - as.numeric(format(jan_first, "%u"))) %% 7
+  
+  sunday_dates <- seq.Date(
+    from = first_sunday, to = as.Date(paste0(year, "-12-31")), by = "week"
+  )
+  
+  month_labels <- lubridate::month(sunday_dates)
+  
+  week_labels <- lubridate::week(sunday_dates)
+  
+  version <- paste(major, month_labels, week_labels, sep = ".")
+  
+  version
+}
+
+
+
+#'
+#' Create a github data release
+#'
+
+paglaom_create_weekly_release <- function(repo = "panukatan/paglaom",
+                                          major) {
+  ## Get release names from GitHub ----
+  release_names <- piggyback::pb_releases()$release_name
+
+  ## Assign release name for this release ----
+  repeat {
+    release_name <- bagyo::get_bagyo()$name
+    if (!release_name %in% release_names) break 
+  }
+    
+  ## Create tag ----
+  tag <- Sys.Date() |>
+    (\(x) x - as.numeric(format(x, "%u")))() |>
+    (\(x)
+      {
+        month_tag <- lubridate::month(x)
+        week_tag  <- lubridate::week(x)
+        tag <- paste(major, month_tag, week_tag, sep = ".")
+        tag
+      }
+    )()
+    
+  
+  ## Create release ----
+  piggyback::pb_release_create(
+    repo = repo, tag = tag, name = release_name
+  )
+  
+  ## Return tag ----
+  tag
+}
+
+
+#'
+#' Create data upload to GitHub
+#'
+
+paglaom_upload_weekly_release <- function(repo = "panukatan/paglaom",
+                                          tag) {
+  zipdir <- tempdir()
+  zip_climate <- file.path(zipdir, "climate.zip")
+  zip_cyclones <- file.path(zipdir, "cyclones.zip")
+  zip_dam <- file.path(zipdir, "dam.zip")
+  zip_heat <- file.path(zipdir, "heat_index.zip")
+  zip_forecasts <- file.path(zipdir, "forecasts.zip")
+  zip_forecasts_agriculture <- file.path(zipdir, "forecasts_agriculture.zip")
+  
+  ## zip climate files ----
+  zip(
+    zip_climate, 
+    files = list.files(
+      path = "data-raw/climate", full.names = TRUE, recursive = TRUE)
+  )
+  
+  ## zip cyclones files ----
+  zip(
+    zip_cyclones, 
+    files = list.files(
+      path = "data-raw/cyclones", full.names = TRUE, recursive = TRUE
+    )
+  )
+  
+  ## zip dam files ----
+  zip(
+    zip_dam, 
+    files = list.files(
+      path = "data-raw/dam", full.names = TRUE, recursive = TRUE
+    )
+  )
+  
+  ## zip heat files ----
+  zip(
+    zip_heat, 
+    files = list.files(
+      path = "data-raw/heat_index", full.names = TRUE, recursive = TRUE
+    )
+  )
+  
+  ## zip forecasts files ----
+  zip(
+    zip_forecasts, 
+    files = list.files(
+      path = "data-raw/forecasts", full.names = TRUE, recursive = TRUE
+    )
+  )
+
+    ## zip agriculture forecasts files ----
+    zip(
+      zip_forecasts_agriculture, 
+      files = list.files(
+        path = "data-raw/forecasts_agriculture", 
+        full.names = TRUE, recursive = TRUE
+      )
+    )
+
+  lapply(
+    X = c(
+      zip_climate, zip_cyclones, zip_dam, zip_heat, zip_forecasts, 
+      zip_forecasts_agriculture
+    ),
+    FUN = piggyback::pb_upload,
+    tag = tag
+  )
+}
+
+
+#'
+#' Get filenames of data from current release
+#' 
+#'
+
+get_data_release_filenames <- function(dataset = NULL) {
+  if (is.null(dataset)) {
+    urls <- piggyback::pb_download_url()
+  } else {
+    urls <- piggyback::pb_download_url(file = paste0(dataset, ".zip"))
+  }
+  
+  destfile <- file.path(tempdir(), basename(urls))
+  
+  Map(
+    f = download.file,
+    url = urls, 
+    destfile = destfile, 
+    mode = "wb"
+  )
+  
+  lapply(
+    X = destfile,
+    FUN = unzip,
+    list = TRUE
+  ) |>
+    dplyr::bind_rows() |>
+    dplyr::pull(Name) |>
+    c()
+  
+  unlink(destfile)
+}
--- R/retrieve_kobo_forms.R
+++ R/retrieve_kobo_forms.R
@@ -1,196 +1,37 @@
 #'
-#' Get KoboToolbox form unique identifier by form name
-#'
-#' This function retrieves the UID (unique identifier) of a KoboToolbox form 
-#' given its form name from an asset list.
-#'
-#' @param asset_list A data frame or tibble containing KoboToolbox assets. This
-#'   is usually produced through a call to [robotoolbox::kobo_asset_list()].
-#' @param form_name A character string specifying the name of the form 
-#'   whose UID is to be retrieved.
-#'
-#' @return A character string representing the UID of the specified form. 
-#'   If the form is not found, a warning is issued and an empty string is
-#'   returned.
-#'
-#' @examples
-#' \dontrun{
-#'   asset_list <- robotoolbox::kobo_asset_list()
-#' 
-#'   kobo_get_uid(asset_list, "Form A")
-#'   kobo_get_uid(asset_list, "Form C")
-#' }
-#'
-#' @export
-#' 
-
-kobo_get_uid <- function(asset_list, form_name) {
-  if (!form_name %in% asset_list$name)
-    warning(
-      "Form with name ", form_name, " not found. ",
-      "Returning empty `uid` string."
-    )
-    
-  ## Get uid ----
-  uid <- asset_list$uid[asset_list$name == form_name]
-
-  ## Return uid ----
-  uid
-}
-
-#'
-#' Get KoboToolbox form version URLs
-#' 
-#' Retrieves a vector of version URLs for a specified KoboToolbox form.
-#'
-#' @param form_id Character. The unique identifier of the KoboToolbox form. This
-#'   can be produced through a call to [kobo_get_uid()].
-#' @param deployed. Logical. Should only the URLs of deployed form versions be
-#'   retrieved? Default to TRUE.
-#'
-#' @returns A character vector of URLs corresponding to the versions of the
-#'   specified form.
-#'
-#' @examples
-#' \dontrun{
-#'   # Replace with your actual form ID
-#'   form_id <- "a1b2c3d4e5f6g7h8i9j0"
-#'   version_urls <- kobo_get_version_urls(form_id)
-#' }
-#'
-#' @export 
-#' 
-
-kobo_get_version_urls <- function(asset_version_list) {
-  asset_version_list <- asset_version_list |>
-    dplyr::filter(deployed)
-  
-  asset_version_urls <- asset_version_list |>
-    dplyr::pull(url) |>
-    split(f = 1:nrow(asset_version_list)) |>
-    stats::setNames(
-      nm = paste0(
-        "v", seq(from = nrow(asset_version_list), to = 1, by = -1)
-      )
-    )
-
-  ## Return urls ----
-  asset_version_urls
-}
-
-#'
-#' Retrieve KoboToolbox form versions
+#' Retrieve KoboToolbox forms from One Drive 
 #' 
 #' 
 
-kobo_archive_form_versions <- function(form_version_url,
-                                       form_title = NULL,
-                                       directory = "forms/archive",
-                                       file_name,
-                                       token = Sys.getenv("KOBOTOOLBOX_TOKEN")) {
-  ## Get version number ----
-  version <- names(form_version_url)
+retrieve_kobo_form <- function(onedrive_form_file,
+                               dest_dir = "forms/release") {
+  onedrive <- Microsoft365R::get_business_onedrive()
 
-  ## Create directory path ----
-  dir_path <- file.path(directory, version)
+  if (!dir.exists(dest_dir)) dir.create(dest_dir)
 
-  ## Create version folder in directory ----
-  if (!dir.exists(dir_path)) dir.create(dir_path, recursive = TRUE)
+  onedrive$download_file(
+    src = onedrive_form_file, 
+    dest = file.path(dest_dir, basename(onedrive_form_file)),
+    overwrite = TRUE
+  )
 
-  ## Create file path ----
-  file_path <- file.path(dir_path, file_name)
-
-  ## Create version as needed ----
-  if (!file.exists(file_path)) {
-    req <- httr2::request(form_version_url[[1]]) |>
-      httr2::req_headers(Authorization = paste0("Token ", token))
-  
-    resp <- req |>
-      httr2::req_perform()
-  
-    form_json <- resp |>
-      httr2::resp_body_json(simplifyVector = TRUE)
-
-    survey <- form_json$content$survey |>
-      dplyr::mutate(
-        type = ifelse(
-          grepl("select_", type), paste(type, select_from_list_name), type
-        ),
-        `label::English (en)` = unlist(label)[2],
-        `label::Creole (cpf)` = unlist(label)[1],
-        `hint::English (en)` = unlist(hint)[2],
-        `hint::Creole (cpf)` = unlist(hint)[1],
-        `constraint_message::English (en)` = unlist(constraint_message)[2],
-        `constraint_message::Creole (cpf)` = unlist(constraint_message)[1]
-      ) |>
-      dplyr::select(
-        -`$kuid`, -`$xpath`, -`$autoname`, -select_from_list_name,
-        -label, -hint, -constraint_message
-      ) |>
-      dplyr::relocate(type, .before = name) |>
-      dplyr::relocate(
-        `label::English (en)`, `label::Creole (cpf)`, 
-        `hint::English (en)`, `hint::Creole (cpf)`,
-        .before = required
-      ) |>
-      dplyr::relocate(
-        `constraint_message::English (en)`, `constraint_message::Creole (cpf)`,
-        .before = relevant
-      )
-
-    choices <- form_json$content$choices |>
-      dplyr::mutate(
-        `label::English (en)` = unlist(label)[2],
-        `label::Creole (cpf)` = unlist(label)[1]
-      ) |>
-      dplyr::relocate(list_name, .before = name) |>
-      dplyr::select(-`$kuid`, -`$autovalue`, -label)
-
-    if ("media::image" %in% names(choices)) {
-      choices <- choices |>
-        dplyr::mutate(
-          `media::image::English (en)` = unlist(`media::image`)[2],
-          `media::image::Creole (cpf)` = unlist(`media::image`)[1]
-        ) |>
-        dplyr::select(-`media::image`)
-    }
-
-    settings <- form_json$content$settings |>
-      dplyr::bind_cols() |>
-      dplyr::mutate(form_title = form_title, .before = style) |>
-      dplyr::rename(form_id = id_string) |>
-      dplyr::relocate(form_id, .before = style)
-  
-    wb <- openxlsx::createWorkbook()
-
-    Map(
-      f = openxlsx::addWorksheet,
-      wb = list(wb, wb, wb),
-      sheetName = c("survey", "choices", "settings")
-    )
-
-    Map(
-      f = openxlsx::writeData,
-      wb = list(wb, wb, wb),
-      sheet = list("survey", "choices", "settings"),
-      x = list(survey, choices, settings)
-    )
-
-    openxlsx::saveWorkbook(wb, file = file_path)
-  }
-
-  ## Return file path ----
-  file_path
-
-  # auth_handle <- curl::new_handle() |>
-  #   curl::handle_setheaders(Authorization = paste0("Token ", token))
-  
-  # curl::curl_download(
-  #   url = req$url,
-  #   destfile = "forms/onco_patient_survey.xls",
-  #   handle = auth_handle
-  # )
+  file.path(dest_dir, basename(onedrive_form_file))
 }
 
 
+retrieve_kobo_media <- function(media_form_directory,
+                                dest_dir = "forms/release") {
+  onedrive <- Microsoft365R::get_business_onedrive()
 
+  dest_dir <- file.path(dest_dir, basename(media_form_directory))
+
+  if (!dir.exists(dest_dir)) dir.create(dest_dir, recursive = TRUE)
+
+  onedrive$download_folder(
+    src = media_form_directory, 
+    dest = dest_dir, 
+    overwrite = TRUE, recursive = TRUE
+  )
+
+  file.path(dest_dir, list.files(dest_dir))
+}
